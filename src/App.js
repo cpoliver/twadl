5f0ea93fa7f92da0ccb4eed4889ba1698736a8ea
Charlie Oliver <charles.oliver@casumo.com>
2017-12-24 12:38:24 +0000

    style(*): Run prettier

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ const App = () => (
     <header className="App-header">
       <h1 className="App-title">Weclome to TWADL</h1>
     </header>
-    <p className="App-intro">
-      The Web App Design Language
-    </p>
+    <p className="App-intro">The Web App Design Language</p>
   </div>
 );
 
